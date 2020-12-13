1892d2f695d17c78a030a3988d88e4c8fc550a2c
Minh Hoang <37230224+nbmhoang@users.noreply.github.com>
2020-12-13 12:42:28 +0700

    Using / endpoint for API

--- app.py
+++ app.py
@@ -13,23 +13,22 @@ data = {
   0: 'Normal',
   1: 'Pneumonia'
 }
-
-@app.route('/index', methods=['POST'])
-def get_img():
-  filestr = request.files['img'].read()
-  img = np.frombuffer(filestr, np.uint8)
-  img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-  img = cv2.resize(img, (224, 224))
-  img = np.expand_dims(img, 0)
-  pred = model.predict(img)
-  pred = np.argmax(np.round(pred), axis=1)
-  return jsonify({
-    'message': data[pred[0]]
-  })
   
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
-  return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    filestr = request.files['img'].read()
+    img = np.frombuffer(filestr, np.uint8)
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (224, 224))
+    img = np.expand_dims(img, 0)
+    pred = model.predict(img)
+    pred = np.argmax(np.round(pred), axis=1)
+    return jsonify({
+      'message': data[pred[0]]
+    })
 
 if __name__ == '__main__':
   # print(app.config['STATIC_FOLDER'])
