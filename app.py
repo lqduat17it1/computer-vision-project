47c59a151bf2bfb732c1b3577f28bc585e15287e
Minh Hoang <nbmhoang.17it1@sict.udn.vn>
2020-12-13 15:38:15 +0700

    Add exception

--- app.py
+++ app.py
@@ -1,35 +1,60 @@
 import cv2
 import numpy as np
+import json
 from keras.models import load_model
 from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 model = load_model('pneumonia_v2.h5')
-data = {
-  0: 'Normal',
-  1: 'Pneumonia'
-}
+with open('labels.json') as f:
+  data = json.load(f)
+
+def allowed_file(filename):
+  # print('filename:', filename)
+  extension = filename.rsplit('.', 1)[1].lower()
+  print('true or false', '.' in filename and extension in ALLOWED_EXTENSIONS)
+  return '.' in filename and extension in ALLOWED_EXTENSIONS
   
 @app.route('/', methods=['GET', 'POST'])
 def home():
   if request.method == 'GET':
     return render_template('index.html')
   else:
-    filestr = request.files['img'].read()
-    img = np.frombuffer(filestr, np.uint8)
-    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, (224, 224))
-    img = np.expand_dims(img, 0)
-    pred = model.predict(img)
-    pred = np.argmax(np.round(pred), axis=1)
-    return jsonify({
-      'message': data[pred[0]]
-    })
+    if 'img' not in request.files:
+      return jsonify({
+        'message': 'Missing file'
+      }), 400
+    file = request.files['img']
+    if file.filename == '':
+      return jsonify({
+        'message': 'No file selected'
+      }), 400
+    
+    if file and allowed_file(file.filename):
+      try:
+        filestr = file.read()
+        img = np.frombuffer(filestr, np.uint8)
+        img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (224, 224))
+        img = np.expand_dims(img, 0)
+        pred = model.predict(img)
+        pred = np.argmax(np.round(pred), axis=1)
+        return jsonify({
+          'message': data[str(pred[0])]
+        })
+      except:
+        return jsonify({
+          'message': 'Server error'
+        }), 500
+    else:
+      return jsonify({
+        'message': 'File doesnt support'
+      }), 400
 
 if __name__ == '__main__':
-  # print(app.config['STATIC_FOLDER'])
   app.run(debug=True)
